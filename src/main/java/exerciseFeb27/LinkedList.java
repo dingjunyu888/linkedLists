12ae5551766e1797aa2fda19136ea3043926c76a
Olga Karpenko <okarpenko@usfca.edu>
2024-02-27 12:35:15 -0800

    Added finding the middle element.

---
+++ src/main/java/exerciseFeb27/LinkedList.java
@@ -0,0 +1,81 @@
+package exerciseFeb27;
+
+/** A class representing a linked list. */
+public class LinkedList {
+	private Node head, tail;
+
+	/** Constructor */
+	public LinkedList() {
+		head = null;
+		tail = null;
+	}
+
+	/**
+	 * Insert a new node with the given element in front of the linked list
+	 * @param elem value of the new node
+	 */
+	public void insertAtFront(int elem) {
+		if (head == null) {
+			head = tail = new Node(elem);
+		}
+		else {
+			Node node = new Node(elem);
+			node.setNext(head);
+			head = node;
+		}
+	}
+
+	/**
+	 * Creates a new node with the given element and adds it to the back of the
+	 * list
+	 */
+	public void append(int elem) {
+		Node newNode = new Node(elem);
+		if (tail != null) {
+			tail.setNext(newNode);
+			tail = newNode;
+		} else {
+			head = tail = newNode;
+		}
+
+	}
+
+	/** Prints all the nodes in the link list */
+	public void printNodes() {
+		Node current = head;
+		while (current != null) {
+			System.out.print(current.getElem() + " ");
+			current = current.next();
+		}
+		System.out.println();
+	}
+
+	/**
+	 * Return the element in the middle of the linked list
+	 * @return value of the middle element
+	 */
+	public int findMiddleElement() {
+		Node slow = head;
+		Node fast = head;
+		// FILL IN CODE: use fast and slow pointers to find the middle node
+
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		LinkedList list2 = new LinkedList();
+		list2.append(1);
+		list2.append(7);
+		list2.append(25);
+		list2.append(50);
+		list2.append(55);
+		list2.append(5);
+		list2.append(13);
+
+		list2.printNodes();
+
+		int midElement = list2.findMiddleElement();
+		System.out.println(midElement);
+	}
+
+}
